276d57f3d92753cf0b8427182281d7ebb4cfb96e
maerjiela <1241665814@qq.com>
2023-07-18 14:53:30 +0800

    feat: 🎸 init footer

---
+++ src/components/footer/FooterCommon.vue
@@ -0,0 +1,70 @@
+<script setup>
+const date = new Date().getFullYear()
+</script>
+
+<template>
+	<footer class="bg-gray-50 pt-20 pb-16 dark:bg-black lg:pt-48 lg:pb-24">
+		<div class="mx-auto max-w-[1280px] px-24 sm2:px-40">
+			<div class="flex flex-col border-b border-gray-line-base pb-26 lg:flex-row lg:pb-56">
+				<div class="mb-30 lg:mr-72 lg:mb-0">
+					<div class="mb-30">
+						<a class="inline-flex items-center space-x-6 text-white" aria-label="LEGENDSTORIES" href="/">
+							<span class="font-bold text-22 tracking-[1.29px] text-white">LEGENDSTORIES</span>
+						</a>
+					</div>
+					<div class="flex space-x-8">
+						<a class="h-40 w-120" href="#" target="_blank" rel="noreferrer noopener">
+							<img class="object-cover" src="@/assets/images/apple-store-badge@3x.png" alt="apple-store-badge" loading="lazy" />
+						</a>
+						<a class="h-40 w-135" href="#" target="_blank" rel="noreferrer noopener">
+							<img class="object-cover" src="@/assets/images/google-play-badge@3x.png" alt="google-play-badge" loading="lazy" />
+						</a>
+					</div>
+				</div>
+				<div class="font-set-sb13 mb-26 text-gray-750 lg:font-set-sb16 dark:text-gray-300 lg:mb-0">
+					<div class="mb-16 flex flex-col space-y-16 lg:mb-16 lg:flex-row lg:space-y-0 lg:space-x-64">
+						<a class="w-140" href="/about">About Us</a>
+						<a class="w-140" href="/news">Announcements</a>
+					</div>
+					<div class="flex flex-col space-y-16 lg:flex-row lg:space-y-0 lg:space-x-64">
+						<a class="w-140" href="/contact-us">Contact Us</a>
+						<a class="w-140" href="https://www.careers-page.com/LEGENDSTORY" target="_blank" rel="noreferrer noopener">Jobs</a>
+					</div>
+				</div>
+				<div class="flex text-gray-750 dark:text-gray-300 lg:ml-auto">
+					<a class="mr-27" target="_blank" rel="noreferrer noopener" href="https://www.facebook.com/WuxiaworldWebnovels/" aria-label="Facebook"
+						><svg width="10" height="21" viewBox="0 0 10 21" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+							<path d="M2.5 7H0v3.5h2.5V21h4.167V10.5h3L10 7H6.667V5.513c0-.788.166-1.138.916-1.138H10V0H6.833C3.833 0 2.5 1.4 2.5 4.025V7Z"></path>
+						</svg>
+					</a>
+					<a class="mr-20" target="_blank" rel="noreferrer noopener" href="https://twitter.com/Wuxiaworld_Ltd" aria-label="Twitter"
+						><svg width="24" height="19" viewBox="0 0 24 19" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+							<path
+								d="M7.548 19c9.057 0 14.01-7.31 14.01-13.647 0-.208-.005-.414-.014-.62A9.88 9.88 0 0 0 24 2.249c-.882.382-1.832.64-2.828.755A4.839 4.839 0 0 0 23.338.351a10.03 10.03 0 0 1-3.127 1.164A4.982 4.982 0 0 0 16.617 0c-2.72 0-4.925 2.148-4.925 4.796 0 .376.043.742.127 1.094C7.727 5.69 4.1 3.78 1.67.879a4.685 4.685 0 0 0-.667 2.41c0 1.664.87 3.133 2.192 3.993a4.988 4.988 0 0 1-2.23-.6c-.002.02-.002.04-.002.06 0 2.324 1.698 4.263 3.95 4.703a5.052 5.052 0 0 1-2.223.082c.627 1.906 2.445 3.293 4.6 3.332a10.048 10.048 0 0 1-6.115 2.053c-.398 0-.79-.022-1.175-.066A14.21 14.21 0 0 0 7.548 19Z"
+							></path>
+						</svg>
+					</a>
+					<a target="_blank" rel="noreferrer noopener" href="https://discord.gg/fSfXuGG6vR" aria-label="Discord"
+						><svg width="24" height="19" viewBox="0 0 24 19" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+							<path
+								d="M20.317 2.156A19.791 19.791 0 0 0 15.432.641a.074.074 0 0 0-.079.037c-.21.375-.444.865-.608 1.25a18.271 18.271 0 0 0-5.487 0 12.643 12.643 0 0 0-.617-1.25.077.077 0 0 0-.079-.037 19.736 19.736 0 0 0-4.885 1.515.07.07 0 0 0-.032.028C.533 6.832-.32 11.366.099 15.844a.082.082 0 0 0 .031.056 19.9 19.9 0 0 0 5.993 3.03.078.078 0 0 0 .084-.028c.462-.63.874-1.295 1.226-1.994.021-.041.001-.09-.041-.106a13.109 13.109 0 0 1-1.872-.892.077.077 0 0 1-.008-.128c.126-.094.252-.192.372-.291a.074.074 0 0 1 .078-.01c3.927 1.792 8.18 1.792 12.061 0a.074.074 0 0 1 .079.009c.12.099.245.198.372.292.044.033.04.1-.006.128-.598.35-1.22.645-1.873.891a.077.077 0 0 0-.041.107c.36.698.772 1.363 1.225 1.993a.076.076 0 0 0 .084.029 19.834 19.834 0 0 0 6.002-3.03.077.077 0 0 0 .032-.055c.5-5.177-.838-9.674-3.549-13.66a.06.06 0 0 0-.031-.029ZM8.02 13.117c-1.182 0-2.157-1.085-2.157-2.419 0-1.333.956-2.418 2.157-2.418 1.21 0 2.176 1.095 2.157 2.418 0 1.334-.956 2.42-2.157 2.42Zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.418 2.157-2.418 1.21 0 2.176 1.095 2.157 2.418 0 1.334-.946 2.42-2.157 2.42Z"
+							></path>
+						</svg>
+					</a>
+				</div>
+			</div>
+			<div class="font-set-r13 flex flex-col items-start justify-center pt-20 text-gray-600 dark:text-gray-400 lg:flex-row lg:items-center lg:pt-24">
+				<div class="mb-8 lg:mb-0">
+					<div>
+						<span>Copyright © </span><a href="/">Legend Stories </a><span>{{ date }}</span>
+					</div>
+				</div>
+				<div class="-mt-8 -ml-8 flex flex-wrap lg:ml-auto">
+					<a class="mt-8 ml-8" href="#"> Terms of Service</a><span class="mt-8 ml-8">·</span>
+					<a class="mt-8 ml-8" href="#">Privacy Policy</a>
+					<span class="mt-8 ml-8">·</span><a class="mt-8 ml-8" href="#">Cookie Policy</a>
+				</div>
+			</div>
+		</div>
+	</footer>
+</template>
